294328abf888debf61ea4a3de13e584a68a5feef
Manoel <manoelfsneto@live.com>
2023-09-08 22:26:09 -0300

    fix: Correção de código

--- controle-fluxo/contador/src/Contador.java
+++ controle-fluxo/contador/src/Contador.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 
-        try {
-            // chamando o método contendo a lógica de contagem
-            contar(parametroUm, parametroDois);
+            try {
+                contar(parametroUm, parametroDois);
 
-        } catch (ParametrosInvalidosException exception) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
-            // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            } catch (ParametrosInvalidosException exception) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+            }
         }
 
     }
@@ -23,13 +22,11 @@ public class Contador {
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
-        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 
         int contagem = parametroDois - parametroUm;
 
-        for (int x = 0; x <= contagem; x++) {
-            System.out.println("Imprimindo o número: " + (x + 1));
+        for (int x = 1; x <= contagem; x++) {
+            System.out.println("Imprimindo o número: " + x);
         }
-        // realizar o for para imprimir os números com base na variável contagem
     }
 }
